9ca5d31ee56ea2bc8459a71af395e02ab05fface
Sendiko <sarangtawon897@gmail.com>
2025-01-12 22:57:45 +0700

    enc(add): setup healthissue

--- models/member_healthIssue.js
+++ models/member_healthIssue.js
@@ -1,5 +1,8 @@
 const { Sql, DataTypes } = require("sequelize");
 const database = require("../config/database");
-const member_healthissue = database.define("member_healthissue");
+const member = require("./member");
+const healthIssue = require("./healthIssue");
+
+const member_healthissue = database.define("member_healthissues");
 
 module.exports = member_healthissue;
